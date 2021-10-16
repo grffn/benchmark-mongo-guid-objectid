3d86b8f2e19c613d9a694407dce25cd2a24d7221
Grigorii Hait <grigoriihait.tirscript.com>
2021-10-17 01:36:15 +0300

    Initial commit

---
+++ Benchmarks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace benchmark_mongo_guid_objectid
+{
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void Scenario1()
+        {
+            // Implement your benchmark here
+        }
+
+        [Benchmark]
+        public void Scenario2()
+        {
+            // Implement your benchmark here
+        }
+    }
+}
